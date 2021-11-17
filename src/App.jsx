54a186888b84c9cefb6a6432ddae7a7c5ca13df6
Fabioexemaciel <76603662+jjjere@users.noreply.github.com>
2021-11-17 13:25:50 -0600

    Pie de pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Layout/Header";
 import "./App.css";
+import Footer from "./Layout/Footer";
 
 function App() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,6 +1,18 @@
 import React from react;
+import "./Footer.css";
+import LinkFooter from "./LinkFooter";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
+import { faShare } from "@fortawesome/free-solid-svg-icons";
 const Footer = () => {
-  return <div>Pie de pagina</div>;
+  return (
+    <div>
+      <footer>
+        <LinkFooter icono={faEnvelope} texto="Contactanos"/>
+        <LinkFooter icono={faShare} texto="Compartir"/>
+      </footer>
+      </div>
+  );
+  
 };
 
 export default Footer;
