bbcb6eb16ade4397d3b2e9f4e7b1e08cc20ad776
Fabioexemaciel <76603662+jjjere@users.noreply.github.com>
2021-11-24 13:37:46 -0600

    pie de pagina

--- src/Layout/Footer.jsx
+++ src/Layout/Footer.jsx
@@ -1,18 +1,17 @@
-import React from react;
+import React from "react";
 import "./Footer.css";
 import LinkFooter from "./LinkFooter";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-import { faShare } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faShare } from "@fortawesome/free-solid-svg-icons";
+
 const Footer = () => {
   return (
     <div>
       <footer>
-        <LinkFooter icono={faEnvelope} texto="Contactanos"/>
-        <LinkFooter icono={faShare} texto="Compartir"/>
+        <LinkFooter icono={faEnvelope} texto="Contactanos" />
+        <LinkFooter icono={faShare} texto="Compartir" />
       </footer>
-      </div>
+    </div>
   );
-  
 };
 
 export default Footer;
