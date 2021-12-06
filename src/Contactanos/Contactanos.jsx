979c2019d9d10962c985a482b33ea2ccfd136d85
Fabioexemaciel <76603662+jjjere@users.noreply.github.com>
2021-12-06 16:50:40 -0600

    contactanos.css

--- src/Contactanos/Contactanos.jsx
+++ src/Contactanos/Contactanos.jsx
@@ -1,8 +1,21 @@
 import react from "react";
 import "./Contactanos.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-const contactanos = () => {
-  
-    
-}
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+const Contactanos = () => {
+  return (
+    <>
+      <div className="Contactanos">
+        <FontAwesomeIcon className="iconosDeContactanos" icon={faFacebook} />
+        <FontAwesomeIcon className="iconosDeContactanos" icon={faInstagram} />
+        <FontAwesomeIcon className="iconosDeContactanos" icon={faEnvelope} />
+      </div>
+      <p className="texto"> hola como estas</p>
+    </>
+  );
+};
 
+export default Contactanos;
